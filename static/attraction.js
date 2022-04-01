1f020befe94ff57db4415338a8224d14d1ad8fc4
chiachil <65595327+chiachil@users.noreply.github.com>
2022-04-02 00:30:55 +0800

    feat: add error page

--- static/attraction.js
+++ static/attraction.js
@@ -12,55 +12,73 @@ renderAttractionInfo(page);
 async function getAttraction(id) {
     return (await fetch(`/api/attraction/${id}`, {method: "GET"})).json();
 };
+// render error
+function renderError(message){
+    let main = document.querySelector('.attraction');
+    let carousel = document.querySelector('.carousel-to-order');
+    let description = document.querySelector('.description');
+    let text = document.createElement('p');
+    carousel.style.display = "none";
+    description.style.display = "none";
+    main.appendChild(text);
+    text.textContent = message;
+    text.classList.add('errorHint');
+}
 
 // render attraction info
 async function renderAttractionInfo(id) {
-    result = await getAttraction(id);
-    let data = result.data;
-    // carousel slide
-    let images = data.images;
-    for (i = 0; i < images.length; i++) {
-        let slide = document.createElement('li');
-        let image = document.createElement('img');
-        let dot = document.createElement('button');
-        if (i > 8) {break;}
-        slide.classList.add('carousel__slide');
-        image.classList.add('carousel__image');
-        image.src = data.images[i];
-        image.alt = data.name;
-        dot.classList.add('carousel__indicator');
-        slide.appendChild(image);
-        track.appendChild(slide);
-        dotsNav.appendChild(dot);
-        if (i == 0){
-            slide.classList.add('current-slide');
-            dot.classList.add('current-slide');
-        }
+    let result = await getAttraction(id);
+    if (result.error){
+        let message = result.message;
+        renderError(message);
     }
-    arrangeSlides();
+    else if (result.data){
+        let data = result.data;
+        // carousel slide
+        let images = data.images;
+        for (i = 0; i < images.length; i++) {
+            let slide = document.createElement('li');
+            let image = document.createElement('img');
+            let dot = document.createElement('button');
+            if (i > 8) {break;}
+            slide.classList.add('carousel__slide');
+            image.classList.add('carousel__image');
+            image.src = data.images[i];
+            image.alt = data.name;
+            dot.classList.add('carousel__indicator');
+            slide.appendChild(image);
+            track.appendChild(slide);
+            dotsNav.appendChild(dot);
+            if (i == 0){
+                slide.classList.add('current-slide');
+                dot.classList.add('current-slide');
+            }
+        }
+        arrangeSlides();
 
-    // order
-    let tour = document.querySelector('.order__tour');
-    let title = document.createElement('h2');
-    let subTitle = document.createElement('h4');
-    title.textContent = data.name;
-    subTitle.textContent = data.category + ' at ' + data.mrt;
-    tour.appendChild(title);
-    tour.appendChild(subTitle);
+        // order
+        let tour = document.querySelector('.order__tour');
+        let title = document.createElement('h2');
+        let subTitle = document.createElement('h4');
+        title.textContent = data.name;
+        subTitle.textContent = data.category + ' at ' + data.mrt;
+        tour.appendChild(title);
+        tour.appendChild(subTitle);
 
-    // description
-    let history = document.querySelector('.description__history');
-    let location = document.querySelector('.description__address');
-    let transports = document.querySelector('.description__transportation');
-    let paragraph = document.createElement('p');
-    let address = document.createElement('p');
-    let transport = document.createElement('p');
-    paragraph.textContent = data.description;
-    address.textContent = data.address;
-    transport.textContent = data.transport;
-    history.appendChild(paragraph);
-    location.appendChild(address);
-    transports.appendChild(transport);
+        // description
+        let history = document.querySelector('.description__history');
+        let location = document.querySelector('.description__address');
+        let transports = document.querySelector('.description__transportation');
+        let paragraph = document.createElement('p');
+        let address = document.createElement('p');
+        let transport = document.createElement('p');
+        paragraph.textContent = data.description;
+        address.textContent = data.address;
+        transport.textContent = data.transport;
+        history.appendChild(paragraph);
+        location.appendChild(address);
+        transports.appendChild(transport);
+    }
 };
 
 // arrange the slides next to one another
